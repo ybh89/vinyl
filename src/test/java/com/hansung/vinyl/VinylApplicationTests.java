6a4b3b553f833a9d25ca5cb596caa940eadf9695
ybh89 <akass191@gmail.com>
2021-07-20 16:06:00 +0900

    build: 프로젝트 생성

---
+++ src/test/java/com/hansung/vinyl/VinylApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.vinyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
