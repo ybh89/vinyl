77f8f3b210b12b0cd34a1c25870f0432e95b8dc1
ybh89 <akass191@gmail.com>
2021-08-23 16:02:59 +0900

    fix: 이메일 전송 비동기 버그 수정

--- src/main/java/com/hansung/vinyl/AppConfig.java
+++ src/main/java/com/hansung/vinyl/AppConfig.java
@@ -3,10 +3,12 @@ package com.hansung.vinyl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableAsync
 @EnableScheduling
 @EnableJpaAuditing
 @Configuration
